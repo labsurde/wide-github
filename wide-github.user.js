ccac22fe00f4eb4956e714b0c8430694fe034951
James McHugh <james.mchugh@GL03967M.local>
2014-11-06 15:00:10 +0000

    First commit

--- wide-github.user.js
+++ wide-github.user.js
@@ -1,23 +1,6 @@
 (function () {
   "use strict";
 
-// ==UserScript==
-// @name        Wide Github
-// @namespace   https://github.com/xthexder/wide-github
-// @description This plugin changes all github repository pages to be full width and dynamically sized.
-// @author      xthexder
-// @copyright   2013+, xthexder (https://github.com/xthexder)
-// @contributor Jason Frey (https://github.com/Fryguy)
-// @contributor Marti Martz (https://github.com/Martii)
-// @license     MIT License; http://opensource.org/licenses/mit-license.php
-// @version     1.0.9
-// @icon        https://raw.githubusercontent.com/xthexder/wide-github/master/icon.png
-// @homepageURL https://github.com/xthexder/wide-github
-// @supportURL  https://github.com/xthexder/wide-github/issues
-// @include     *github.com*
-// @grant       none
-// ==/UserScript==
-
   if (document.getElementById('js-repo-pjax-container')) {
     var s = document.createElement('style');
     s.type = "text/css";
